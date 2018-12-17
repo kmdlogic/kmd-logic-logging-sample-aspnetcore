965c67409784cbb4b7d712c3df7333e535ac76c8
Adam Chester <adamchester@gmail.com>
2018-12-17 12:02:51 +1000

    Add some logging middleware

---
+++ src/Kmd.Logic.Logging.Sample.AspnetCore/Middleware/SerilogUserContextMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Serilog.Context;
+
+// NOTE: purposely in no namespace to keep log noise to a minimum
+
+/// <summary>
+/// Enriches the Serilog <see cref="LogContext" /> with "UserIdentityName" pulled
+/// from <see cref="HttpContext.User" /> on each request, only if 
+/// <see cref="System.Security.Principal.IIdentity.IsAuthenticated" /> is <c>true</c>.
+/// </summary>
+public class SerilogUserContextMiddleware
+{
+    private readonly RequestDelegate _next;
+    public SerilogUserContextMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext httpContext)
+    {
+        var identity = httpContext?.User?.Identity;
+        var username = identity?.Name;
+        if (identity?.IsAuthenticated == true)
+        {
+            using (LogContext.PushProperty("UserIdentityName", username))
+            {
+                await _next(httpContext);
+            }
+        }
+        else
+        {
+            await _next(httpContext);
+        }
+    }
+}
